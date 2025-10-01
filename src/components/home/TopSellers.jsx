52284cd6bcabb3758608445c1ca9e414365ac31e
JerMontoya <jeralyn.haycock@gmail.com>
2025-10-01 07:59:23 -0800

    pull from api

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,42 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import "./TopSellers.css"
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function getTopSellers() {
+    setLoading(true);
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`
+    );
+    setTimeout(() => {
+      setTopSellers(data);
+      setLoading(false);
+
+  }, 2000)
+  }
+
+  useEffect(() => {
+    getTopSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Top Sellers</h2>
-              <div className="small-border bg-color-2"></div>
+      {loading ? (
+        <div className="skeleton__loading">
+          <div className="skeleton__title"></div>
+          <div className="small-border bg-color-2"></div>
+          <div className="col-md-12">
+            <div className="author_list">
+              
             </div>
           </div>
-          <div className="col-md-12">
-            <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
-            </ol>
+        </div>
+      ) : (
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="text-center">
+                <h2>Top Sellers</h2>
+                <div className="small-border bg-color-2"></div>
+              </div>
+            </div>
+            <div className="col-md-12">
+              <ol className="author_list">
+                {topSellers.map((topSeller, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${topSeller.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={topSeller.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${topSeller.authorId}`}>
+                        {topSeller.authorName}
+                      </Link>
+                      <span>{topSeller.price} ETH</span>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };

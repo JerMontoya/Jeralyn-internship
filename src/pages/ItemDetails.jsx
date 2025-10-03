183b6040977a8d3e9f779a8cb6ea711dce13acbf
JerMontoya <jeralyn.haycock@gmail.com>
2025-10-02 17:34:15 -0800

    call API, make dynamic

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,10 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import LoadingState from "../components/LoadingState";
 
 const ItemDetails = () => {
+  const { Id } = useParams();
+  const [details, setDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getDetails() {
+      const { data } = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${Id}`
+      );
+      setDetails(data);
+      setLoading(false);
+    }
+    getDetails();
+  }, [Id]);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -13,28 +28,31 @@ const ItemDetails = () => {
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
+        <LoadingState loading={loading}>
+          {details && (
+
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
             <div className="row">
               <div className="col-md-6 text-center">
                 <img
-                  src={nftImage}
+                  src={details.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
                   alt=""
                 />
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>{details.title} #{details.tag}</h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
                       <i className="fa fa-eye"></i>
-                      100
+                      {details.views}
                     </div>
                     <div className="item_info_like">
                       <i className="fa fa-heart"></i>
-                      74
+                      {details.likes}
                     </div>
                   </div>
                   <p>
@@ -48,12 +66,12 @@ const ItemDetails = () => {
                       <div className="item_author">
                         <div className="author_list_pp">
                           <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                            <img className="lazy" src={details.ownerImage} alt="" />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to="/author">{details.ownerName}</Link>
                         </div>
                       </div>
                     </div>
@@ -65,12 +83,12 @@ const ItemDetails = () => {
                       <div className="item_author">
                         <div className="author_list_pp">
                           <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                            <img className="lazy" src={details.creatorImage} alt="" />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to="/author">{details.creatorName}</Link>
                         </div>
                       </div>
                     </div>
@@ -78,7 +96,7 @@ const ItemDetails = () => {
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <span>{details.price}</span>
                     </div>
                   </div>
                 </div>
@@ -86,6 +104,9 @@ const ItemDetails = () => {
             </div>
           </div>
         </section>
+          )}
+
+        </LoadingState>
       </div>
     </div>
   );
